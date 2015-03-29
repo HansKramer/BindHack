e336bf32067f403f017114e6aa3a927ad132e847
Hans Kramer <hans@Hanss-MacBook-Pro-2.local>
2015-03-29 23:30:43 +0200

    Development check-in

--- tdns.go
+++ tdns.go
@@ -17,10 +17,6 @@ import "bytes"
 import "io"
 
 
-type DNSServer struct {
-    addr *net.UDPAddr
-}
-
 type DNSHeader struct {
     id      uint16
     status  uint16
@@ -30,42 +26,105 @@ type DNSHeader struct {
     arcount uint16
 }
 
+type DNSQuestion struct {
+    qname  []string
+    qtype  uint16
+    qclass uint16 
+}
 
-func (header DNSHeader) Init(r io.ByteReader) {
-    binary.Read(r, binary.BigEndian, &id)   
-    binary.Read(r, binary.BigEndian, &status)   
-    binary.Read(r, binary.BigEndian, &qdcount)   
-    binary.Read(r, binary.BigEndian, &ancount)   
+type DNSAnswer struct {
+    name     []string
+    type     uint16
+    class    uint16 
+    ttl      uint16
+    rdlength uint16
+    rdata    []byte
+}
+
+type DNSAnswer struct {
+    name     []string
+    type     uint16
+    class    uint16 
+    ttl      uint16
+    rdlength uint16
+    rdata    []byte
+}
+
+type DNSServer struct {
+    addr     *net.UDPAddr
+    header   DNSHeader
+    question DNSQuestion 
+    answer   DNSAnswer
+}
+
+
+func (header *DNSHeader) Init(r io.Reader) {
+    binary.Read(r, binary.BigEndian, &header.id)
+    binary.Read(r, binary.BigEndian, &header.status)
+    binary.Read(r, binary.BigEndian, &header.qdcount)
+    binary.Read(r, binary.BigEndian, &header.ancount)
+    binary.Read(r, binary.BigEndian, &header.nscount)
+    binary.Read(r, binary.BigEndian, &header.arcount)
+}
+
+func (question *DNSQuestion) Init(s *bytes.Buffer) {
+    var oct_len uint8
+    var data    string
+    question.qname = nil
+    for {
+        binary.Read(io.Reader(s), binary.BigEndian, &oct_len)
+        if oct_len == 0 {
+            binary.Read(io.Reader(s), binary.BigEndian, &question.qtype)
+            binary.Read(io.Reader(s), binary.BigEndian, &question.qclass)
+            return
+        }
+        fmt.Printf("oct_len : %d\n",  int64(oct_len))
+        data = string(s.Next(int(oct_len)))
+        question.qname = append(question.qname, data)
+    }
+}
+
+func (header DNSHeader) GetField(field string) uint16 {
+    if field == "ID" {
+        return header.id
+    }
+    if field == "QR" {
+        return (header.status & 0x8000) >> 15
+    }
+    return 0
 }
 
 
 func (dns DNSServer) Run() {
-    dns.addr, _ = net.ResolveUDPAddr("udp", ":53")
-    sock, _ := net.ListenUDP("udp", dns.addr)
+    dns.addr, _  = net.ResolveUDPAddr("udp", ":53")
+    sock, _     := net.ListenUDP("udp", dns.addr)
 
     var buf [1024]byte
 
     for {
         rlen, remote, err := sock.ReadFromUDP(buf[:])
+
         fmt.Println(buf)
-        r := bytes.NewBuffer(buf[0:6])
-        var header DNSHeader
-        header.Init(&r)
+        r := bytes.NewBuffer(buf[:])
+        dns.header.Init(r)
+        if dns.header.qdcount > 0 {  // for now only handle a single question requests 
+            dns.question.Init(r)
+        }
+        if dns.header.ancount > 0 {
+            dns.
+        } 
  
-//        var id, status, qdcount, ancount uint16
-//        binary.Read(r, binary.BigEndian, &id)   
-//        binary.Read(r, binary.BigEndian, &status)   
-//        binary.Read(r, binary.BigEndian, &qdcount)   
-//        binary.Read(r, binary.BigEndian, &ancount)   
-//        binary.Read(r, binary.BigEndian, &nscount)   
-//        binary.Read(r, binary.BigEndian, &arcount)   
 //        fmt.Printf("header : %04X %04X %04X %04X\n", id, status, qdcount, ancount)
         fmt.Println(remote)
         fmt.Println(err)
         fmt.Println(rlen)
+        fmt.Println(dns.header.GetField("ID"))
+        fmt.Println(dns.header.GetField("QR"))
+        fmt.Println(dns.question)
+        
+        fmt.Println(dns.header.ancount)
     }
 }
-
 
 
 func main() {
